0d2cd6e6fa4cd197d927d6c9951027a6dde85528
zhangcm <zhangcm0719@163.com>
2017-10-19 17:25:12 +0800

    创建项目结构

---
+++ java-sdk-third/java-sdk-third-weixin/src/main/java/com/zhcmi/java/sdk/third/weixin/Code.java
@@ -0,0 +1,34 @@
+package com.zhcmi.java.sdk.third.weixin;
+
+/**
+ * Created by xhw on 2017/09/06.
+ */
+public class Code {
+
+    private Integer code;
+    private String message;
+
+    public Code(){
+    }
+
+    public Code(Integer code, String message){
+        this.code = code;
+        this.message = message;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
